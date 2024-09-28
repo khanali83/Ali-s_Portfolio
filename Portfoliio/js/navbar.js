a3c445e8ec161e177d895807cd76a174c8c3e334
KHAN ALI ABDUL REHMAN <162101818+khanali83@users.noreply.github.com>
2024-09-29 02:38:28 +0530

    Update navbar.js

--- Portfoliio/js/navbar.js
+++ Portfoliio/js/navbar.js
@@ -1,1 +1,7 @@
+const mobileMenu = document.getElementById("mobile-menu");
+const navbarNav = document.querySelector(".navbar-nav");
+
+mobileMenu.addEventListener("click", () => {
+    navbarNav.classList.toggle("show");
+});
 
